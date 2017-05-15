882a72b99783f3c8dd3c35602c1f9184bc02b4bf
Rostyk <grandbloodycf@gmail.com>
2017-05-15 18:32:03 +0300

    Homework12

---
+++ Assets/Script/DestroyOnContact.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnContact : MonoBehaviour {
+
+    // Use this for initialization 
+    public GameObject explosion;
+   
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+        Destroy(collision.gameObject);
+        Instantiate(explosion, transform.position, transform.rotation);
+        }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
